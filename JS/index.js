f5f6f1df89054d3d850604bdb289dc5e3f1a0e0f
Heather Kusmierz <heather.kusmierz@gmail.com>
2017-10-31 11:49:58 -0400

    Refactor React code to remove React.createClass syntax

--- JS/index.js
+++ JS/index.js
@@ -6,6 +6,7 @@ var ReactDOM = require('react-dom');
 
 
 // TODO: Update React code
+/*
 var CodeInput = React.createClass({
   handleChange: function() {
     this.props.onUserInput(
@@ -71,10 +72,96 @@ var Previewer = React.createClass({
     );
   }
 });
+*/
 
-ReactDOM.render(<Previewer />, document.getElementById("react-container"));
 
-/* TEST MARKDOWN CODE
+class CodeInput extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange() {
+    this.props.onUserInput(
+      this.refs.codeTextInput.value
+    );
+  }
+
+  render() {
+    return (
+      <div className="col-sm-6 col-xs-12">
+        <h4 className="header">Input Your Markdown Code:</h4>
+        <textarea className="form-control"
+          value={this.props.codeText}
+          ref="codeTextInput"
+          onChange={this.handleChange}
+        />
+      </div>
+    );
+  }
+};
+
+
+class CodeOutput extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="col-sm-6 col-xs-12">
+        <h4 className="header">Code Preview:</h4>
+        <div
+          className="codeOut text-left"
+          dangerouslySetInnerHTML={{__html: this.props.markedOut}}
+        />
+      </div>
+    );
+  }
+};
+
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      codeText: "",
+      markedOut: ""
+    }
+
+    this.handleInput = this.handleInput.bind(this);
+  }
+
+  handleInput(codeText) {
+    var markedHTML = marked(codeText, {sanitize: true});
+    //console.log(markedHTML);
+    this.setState({
+      codeText: codeText,
+      markedOut: markedHTML
+    });
+  }
+
+  render() {
+    return (
+      <div className="row text-center">
+        <CodeInput
+          codeText={this.state.codeText}
+          onUserInput={this.handleInput}
+        />
+        <CodeOutput
+          markedOut={this.state.markedOut}
+        />
+      </div>
+    );
+  }
+};
+
+
+ReactDOM.render(<App />, document.getElementById("react-container"));
+
+/*
+TEST MARKDOWN CODE:
 Heading
 =======
 
